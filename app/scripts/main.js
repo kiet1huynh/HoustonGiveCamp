f52baabb7f71d94ea440982e785cba77213c59d3
Eric Huang <wongngaikei@gmail.com>
2015-03-05 03:16:50 -0600

    jshint is so strict

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,30 @@
 $(function() {
+  'use strict';
 
-    function ContactFormViewModel() {
-      var self = this;
-      self.name = ko.observable();
-      self.email = ko.observable();
-      self.message = ko.observable();
+  function ContactFormViewModel() {
+    var self = this;
+    self.name = ko.observable();
+    self.email = ko.observable();
+    self.message = ko.observable();
 
-      self.submit = function() {
-        $.ajax({
-          url: 'https://docs.google.com/forms/d/1sQjXasjAs7WDMLBX0sKzsGGtnB7yncBufSHHmgcRu3E/formResponse',
-          data: {"entry_886735557": self.name(), "entry_85346771": self.email(), "entry_105022985":self.message() },
-          type: 'POST',
-          statusCode: {
-            0: function() {
-              toastr.success('Your Message has been submitted!');
-            },
-            200: function() {
-              toastr.success('Your Message has been submitted!');
-            }
+    self.submit = function() {
+      $.ajax({
+        url: 'https://docs.google.com/forms/d/1sQjXasjAs7WDMLBX0sKzsGGtnB7yncBufSHHmgcRu3E/formResponse',
+        data: {'entry_886735557': self.name(), 'entry_85346771': self.email(), 'entry_105022985': self.message() },
+        type: 'POST',
+        statusCode: {
+          0: function() {
+            toastr.success('Your Message has been submitted!');
+          },
+          200: function() {
+            toastr.success('Your Message has been submitted!');
           }
-        });
-      }
-    }
+        }
+      });
+    };
+  }
 
-    var vm = new ContactFormViewModel();
+  var vm = new ContactFormViewModel();
 
-    ko.applyBindings(vm);  
+  ko.applyBindings(vm);
 });
